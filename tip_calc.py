b82525559fd91fefd342aa9d5dfaf8d1b6bd3982
jsongallagher <jasong13@gmail.com>
2023-10-05 12:33:15 -0600

    Code comment formatting

--- tip_calc.py
+++ tip_calc.py
@@ -1,15 +1,15 @@
 print("Welcome to the tip calculator ")
-#get bill total
+#Get bill total
 total_bill = float(input("What was the total bill? $"))
 
-#get tip, calc new bill total
+#Get tip, calc new bill total
 percent_tip = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
 bill_plus_tip = total_bill + (total_bill * (percent_tip / 100))
 
-#get number of guests splitting bill
+#Get number of guests splitting bill
 split = int(input("How many people to split the bill? "))
 bill_per_person = (bill_plus_tip / split)
 bill_per_person = round(bill_per_person, 2) #round 2 decimal places
 
-#display bill per person
+#Display bill per person
 print(f"Each person should pay: ${bill_per_person}")
